8a426cc5705bc29018e12ebc5685d65ea2642710
Andrew Stangl <andrewstangl@gmail.com>
2012-05-15 21:47:33 +0000

    remove duplicates correctly with sets

--- check_file_recursive/check_file_recursive.py
+++ check_file_recursive/check_file_recursive.py
@@ -8,7 +8,6 @@ from glob import glob
 
 foundfiles = []
 dirlist = []
-filelist = []
 
 search_here = '/opt/scribe/storage'
 narrow_search = 'current'
@@ -19,130 +18,81 @@ exclude_this_too = 'client'
 __version__ = '0.0.1'
 
 def find_all_files(search_location, sub_search = None):
+
+    file_list = []
+    abs_path_list = []
     for root, dirs, files in os.walk(search_location):
         for subdir in dirs:
             dirlist.append(os.path.join(root, subdir))
         for filename in files:
-            filelist.append(os.path.join(root, filename))
+            abs_path_list.append(os.path.join(root, filename))
+            file_list.append(filename)
 
-    all_files = filelist
+    all_files = file_list
     file_set = set(all_files)
 
     all_dirs = dirlist
     dir_set = set(all_dirs)
 
-    return file_set
+    all_files_abs = abs_path_list
+
+    return all_files, all_files_abs
 
 ### the following are filter functions:
 
-def include_files(all_files, inc_files1, inc_files2 = None):
+def include_files(all_files, inc_files):
+
     included_list = []
+    included_list1 = []
     included_list2 = []
-    print "search_term:" + inc_files1
-    if inc_files2:
-        print "another filter:" + inc_files2
-    time.sleep(2)
 
-    # first search term
-    for file in all_files:
-        if inc_files1 in file:
-            included_list.append(file)
+    if len(inc_files) == 1:
+        for file in all_files:
+            if inc_files[0] in file:
+                included_list.append(file)
+    elif len(inc_files) == 2:
+        for file in all_files:
+            if inc_files[0] in file:
+                included_list1.append(file)
+        for file in all_files:
+            if inc_files[1] in file:
+                included_list2.append(file)
+        included_list = set(included_list1) & set(included_list2) 
+    else: 
+        print "only two search terms allowed"
 
-    included_set = set(included_list)
+    uniq_inc_files = list(set(included_list))
 
-    # If we need another search term:
-    if inc_files2 is not None:
-        for file2 in all_files:
-            if inc_files2 in file2:
-                included_list2.append(file2)
-    try:
-        included_list2
-    except NameError:
-        print ""
-    else:
-        included_set2 = set(included_list2)
-        final_set = included_set & included_set2
-#        for fs in final_set:
-#            print fs
-#            time.sleep(1)
-        return final_set
-    
-#    for fs in included_set:
-#        print fs
-#        time.sleep(1)
-#
-    final_set = included_set
-    return final_set
+    final_list = uniq_inc_files
+    return final_list
 
-def exclude_files(all_files, exc_files1, exc_files2 = None):
+def exclude_files(all_files, exc_files):
+
     excluded_list = []
+    excluded_list1 = []
     excluded_list2 = []
-    print "exclude term:" + exc_files1
-    if exc_files2:
-        print "another exclusion:" + exc_files2
-    time.sleep(2)
 
-    # first exclusion term
-    for file in all_files:
-        if exc_files1 in file:
-            excluded_list.append(file)
+    if len(exc_files) == 1:
+        for file in all_files:
+            if exc_files[0] in file:
+                excluded_list.append(file)
+    elif len(exc_files) == 2:
+        for file in all_files:
+            if exc_files[0] in file:
+                excluded_list1.append(file)
+        for file in all_files:
+            if exc_files[1] in file:
+                excluded_list2.append(file)
+        excluded_list = set(excluded_list1) | set(excluded_list2) 
+    else: 
+        print "only two search terms allowed"
 
-    excluded_set = set(excluded_list)
+    uniq_exc_files = list(set(excluded_list))
 
-    # If we need another exclusion:
-    if exc_files2 is not None:
-        for file2 in all_files:
-            if exc_files2 in file2:
-                excluded_list2.append(file2)
-    else:
-#        for fs in excluded_set:
-#            print fs
-#            time.sleep(1)
+    final_list = uniq_exc_files
+    return final_list
 
-        final_set = excluded_set
-        return final_set
 
-    try:
-        excluded_list2
-    except NameError:
-        print ""
-    else:
-        excluded_set2 = set(excluded_list2)
-        final_set = excluded_set & excluded_set2
-#        for fs in final_set:
-#            print fs
-#            time.sleep(1)
-        return final_set
-
-#matched_dir = []
-#exc_files = []
-
-#file_list = {}
-#final_files = {}
-#final_files2 = {}
-#
-#file_list = find_all_files(search_here)
-#
-#inclusions_set = include_files(file_list, narrow_search, narrow_search_more)
-#exclusions_set = exclude_files(file_list, exclude_this)
-
-#print "Inclusions:"
-#for file in inclusions_set:
-#    print file
-##    time.sleep(.1)
-#
-#print "Exclusions:"
-#for file in exclusions_set:
-#    print file
-##    time.sleep(.1)
-#
-#return_set = inclusions_set - exclusions_set
-#
-#print "Final set:"
-#for file in return_set:
-#    print file
-##    time.sleep(.5)
-#
 #for f in final_files:
 #    print f
 #    time.sleep(0.2)
@@ -175,16 +125,35 @@ def convert_bytes(bytes):
         size = '%.2fb' % bytes
     return size
 
-def run_check(path, check_size, check_time, _filter, _exclude):
+def run_check(path, check_size, check_time, filter, exclude):
 
-    file_list = find_all_files(path)
+    (file_list, file_list_abs) = find_all_files(path)
 
-    inclusions_set = include_files(file_list, _filter)
-    exclusions_set = exclude_files(file_list, _exclude)
+    if filter is not None:
+        inclusions_set = set(include_files(file_list, filter))
+        #print inclusions_set
+    else:
+        inclusion_set = set()
+
+    if exclude is not None: 
+        exclusions_set = set(exclude_files(file_list, exclude))
+        #print exclusions_set
+    else:
+        excusion_set = set()
 
     final_set = inclusions_set - exclusions_set
-    print final_set
-    
+
+    final_list = list(final_set)
+    final_list.sort()
+
+    for file in final_list:
+        print file
+
+    num_items = len(list(final_set))
+
+    print "num items:"
+    print num_items
+   
 
 if __name__ == "__main__":
     import optparse
@@ -214,29 +183,29 @@ if __name__ == "__main__":
 
 #    if not options.mtime or options.size:
 #        parser.error("No attribute specified - we need something to check - mtime or size is required")
-#
+
     if options.mtime and options.size:
         parser.error("Unfortunately you can't check both size and time at once (just yet)")
 
     if options.exclude:
         _exclude = [f.strip() for f in options.exclude.split(',')]
     else: 
-        _exclude = False
+        _exclude = ""
 
     if options.filter:
         _filter = [f.strip() for f in options.filter.split(',')]
     else: 
-        _filter = False
+        _filter = ""
 
     if options.mtime:
         _check_time = options.mtime
     else:
-        _check_time = False
+        _check_time = ""
 
     if options.size:
         _check_size = options.size
     else:
-        _check_size = False
+        _check_size = ""
 
     run_check(options.path, _check_size, _check_time, _filter, _exclude)
 
