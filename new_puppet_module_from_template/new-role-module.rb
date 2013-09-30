b984ac5f8420f22178d83acecda6c4f206740e33
Andrew Stangl <andrewstangl@gmail.com>
2013-09-30 09:08:16 +0100

    new role module generation from template, and use HEREOC instead of quoted string

--- new_puppet_module_from_template/new-role-module.rb
+++ new_puppet_module_from_template/new-role-module.rb
@@ -14,7 +14,13 @@ dirnames.each { |dir|
 }
 
 template = <<-'EOF'
-class <%= module_name %>::params {
+# Role Module: <%= module_name %>
+ 
+class <%= module_name %> {
+
+    include <%= module_name %>::install
+    include <%= module_name %>::config
+    include <%= module_name %>::service
 
 }
 
@@ -31,57 +37,33 @@ class <%= module_name %>::install (
 
 }
 
-class <%= module_name %>::service (
-
-    ) inherits <%= module_name %>::params {
-
-    # Defaults for all services:
-    Service {
-        require => Class[<%= module_name %>::config],
-        ensure  => running,
-        enable  => true
-    }
-
-    # Specific services:
-
-}
-
-class <%= module_name %>::config (
-
-    ) inherits <%= module_name %>::params {
+class <%= module_name %>::config {
 
     # Defaults for all files in here:
     File { 
-        require => Class[<%= module_name %>::install],
-        notify  => Class[<%= module_name %>::service],
+        require => Class["<%= module_name %>::install"],
+        notify  => Class["<%= module_name %>::service"],
         owner   => root,
         group   => root,
         mode    => 644
     }
 
     # Specific files
-    # (use content => template(<%= module_name %>/<path>) to use templates from
+    # (use content => template("<%= module_name %>/<path>") to use templates from
     #  this module).
 
 }
 
-#
-# Class: <%= module_name %>
-#
-# Description of <%= module_name %>
-#
+class <%= module_name %>::service {
 
-class <%= module_name %> (
-
-    ) inherits <%= module_name %>::params {
-
-    include <%= module_name %>::install
-
-    class { '<%= module_name %>::config':
-
+    # Defaults for all services:
+    Service {
+        require => Class["<%= module_name %>::config"],
+        ensure  => running,
+        enable  => true
     }
 
-    include <%= module_name %>::service
+    # Specific services:
 
 }
 EOF
